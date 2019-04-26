2efc184437f549476cb8fab118a7392671e2782d
HenryKing <stlblues.henry@gmail.com>
2019-04-26 10:40:10 -0400

    more classes

--- convert.py
+++ convert.py
@@ -4,7 +4,7 @@ import os
 from os import walk, getcwd
 from PIL import Image
 
-classes = ["001","002","003","004","005","006"]
+classes = ["001","002","003","004","005","006","007","008"]
 
 def convert(size, box):
     dw = 1./size[0]
@@ -24,7 +24,7 @@ def convert(size, box):
 
 """ Configure Paths"""
 
-folder = "005"
+folder = "008"
 
 mypath = "./Labels/" + folder + "/"
 outpath = "./Images/" + folder + "/"
--- process.py
+++ process.py
@@ -12,9 +12,9 @@ file_test = open('test.txt', 'w')
 
 # Populate train.txt and test.txt
 
-for pathAndFilename in glob.iglob(os.path.join(current_dir+"/Images/002", "*.jpg")):
+for pathAndFilename in glob.iglob(os.path.join(current_dir+"/Images/008", "*.jpg")):
     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
-    file_train.write("./Images/002/" + title + '.jpg' + "\n")
+    file_train.write("./Images/008/" + title + '.jpg' + "\n")
 
 for pathAndFilename2 in glob.iglob(os.path.join(current_dir+"/Test", "*.jpg")):
     title, ext = os.path.splitext(os.path.basename(pathAndFilename2))
